00d276cc9f882ae4c7d632ab0a0ba10b1527fc75
Product Market <productmarket.biz@gmail.com>
2026-01-11 10:44:26 -0800

    changes to main

--- hello.py
+++ hello.py
@@ -1,4 +1,21 @@
 print("Hello, I am a venemous Python myself")
 name = "Cal" 
 age = 20
-print("My name is",name,"and I am",age, "years old")
+city = "London"
+print("My name is",name,"and I am",age,"years old and I live in",city)
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits[0])
+fruits.append("fig")
+print("I like",fruits)
+for fruit in fruits:
+    print("I like",fruit)
+food = ["pizza", "hamburger", "hotdog"]
+for food in food:
+    print("I like",food)
+
+age = 20
+if age>= 18: 
+    print("You are an adult")
+else:
+    print("You are a minor")
+    
