73025164d7ccb41c303689826d7e34c8757d1a9d
Product Market <productmarket.biz@gmail.com>
2026-01-11 10:18:56 -0800

    changes

--- hello.py
+++ hello.py
@@ -2,4 +2,14 @@ print("Hello, I am a venemous Python myself")
 name = "Cal" 
 age = 20
 city = "London"
-print("My name is",name,"and I am",age, "years old and I live in",city)
+print("My name is",name,"and I am",age,"years old and I live in",city)
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits[0])
+fruits.append("fig")
+print("I like",fruits)
+for fruit in fruits:
+    print("I like",fruit)
+food = ["pizza", "hamburger", "hotdog"]
+for food in food:
+    print("I like",food)
+
