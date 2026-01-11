05b6400c1d27f758501c9c884c380a932c84c660
Product Market <productmarket.biz@gmail.com>
2026-01-11 11:12:11 -0800

    if else included

--- hello.py
+++ hello.py
@@ -10,12 +10,24 @@ print("I like",fruits)
 for fruit in fruits:
     print("I like",fruit)
 food = ["pizza", "hamburger", "hotdog"]
-for food in food:
-    print("I like",food)
+for foods in food:
+    print("I like",foods)
 
 age = 20
 if age>= 18: 
     print("You are an adult")
 else:
     print("You are a minor")
-    
+
+if food[0]=="pizza":
+    print("I love",food[0])
+else:
+    print("I love",food[1])
+
+for foods in food:
+    if foods == "pizza":
+        print("I found",foods,"in the list")
+    else:
+        print("I found",foods,"in the list instead")
+
+
