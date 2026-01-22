a51630e305fbb5ce9b851762e83b4079ce5f915e
Product Market <productmarket.biz@gmail.com>
2026-01-22 09:42:44 -0800

    Basic Functions d2 revisions

--- hello.py
+++ hello.py
@@ -1,4 +1,38 @@
 print("Hello World!")
 name = "Kaushik"
-age = 20
+age = 17
 print("My name is",name,"and I am",age,"years old")
+fruits = ["apple","orange","banana","cherry"]
+print(fruits[0])
+fruits.append("berry")
+print(fruits)
+
+for a in fruits:
+    print("I like",a)
+
+if age>= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+for b in fruits:
+    if b == "apple":
+        print ("I found",b)
+    else:
+        print("No we did not find apples but found",b)
+
+def greet(x1):
+    print("Hello",x1)
+
+greet("Kaushik")
+
+
+def square(b):
+    return b*b
+print (square(5))
+
+
+
+
+
+  
